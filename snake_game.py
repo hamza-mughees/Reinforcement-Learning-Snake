7de267438a37086666305b64d0f952c3a21bb8cb
hamza-mughees <mugheesh@tcd.ie>
2021-04-27 03:08:52 +0100

    Basic pygame canvas with inits

--- snake_game.py
+++ snake_game.py
@@ -15,8 +15,41 @@ settings = {
     'speed': 20,
 }
 
+Point = namedtuple('Point', ['x', 'y'])
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+
 class Direction(Enum):
     RIGHT = 1
     LEFT = 2
     UP = 3
     DOWN = 4
+
+class SnakeGame:
+    def __init__(self, width=640, height=480):
+        self.width = width
+        self.height = height
+
+        self.display = pygame.display.set_mode(
+                        (self.width, self.height))
+        pygame.display.set_caption('Snake Game')
+        self.clock = pygame.time.Clock()
+
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.width/2, self.height/2)
+        self.snake = [self.head,
+                      Point(self.head.x - settings['block_size'], self.head.y),
+                      Point(self.head.x - 2*settings['block_size'], self.head.y)]
+        
+        self.food = None
+        self.score = 0
+
+        self._place_food()
+    
+    def _place_food(self):
+        pass
+
+if __name__ == '__main__':
+    game = SnakeGame()
