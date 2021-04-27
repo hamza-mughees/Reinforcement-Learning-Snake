492960e0c428b9e337dbe532c1a665da7c45106a
hamza-mughees <mugheesh@tcd.ie>
2021-04-28 00:13:13 +0100

    Displaying training graph

--- plotting.py
+++ plotting.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from IPython.display import clear_output, display
 
 plt.ion()
+plt.show()
 
 def plot(scores, mean_scores):
     clear_output(wait=True)
@@ -11,7 +12,7 @@ def plot(scores, mean_scores):
     plt.xlabel('Episodes (games)')
     plt.ylabel('Score')
     plt.ylim(ymin=0)
-    plt.plot(scores)
-    plt.plot(mean_scores)
-    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
-    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.plot(scores, color='orange', label='Score')
+    plt.plot(mean_scores, color='green', label='Mean score')
+    plt.legend()
+    plt.pause(0.001)
