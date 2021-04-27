be7e7068e0d0b8b98009df0bbc7477095e1166ef
hamza-mughees <mugheesh@tcd.ie>
2021-04-27 05:45:25 +0100

    Displaying snake and food

--- snake_game.py
+++ snake_game.py
@@ -7,7 +7,8 @@ colours = {
     'black': (0,0,0),
     'white': (255,255,255),
     'red': (200,0,0),
-    'green': (0,200,0)
+    'green_big': (0,250,0),
+    'green_small': (0,200,0)
 }
 
 settings = {
@@ -47,9 +48,44 @@ class SnakeGame:
         self.score = 0
 
         self._place_food()
+        self._update()
+    
+    def _update(self):
+        self.display.fill(colours['black'])
+
+        block_border_size = 2
+
+        for p in self.snake:
+            pygame.draw.rect(self.display, 
+                            colours['green_big'],
+                            pygame.Rect(p.x, p.y, 
+                                    settings['block_size'], 
+                                    settings['block_size']))
+            pygame.draw.rect(self.display, 
+                            colours['green_small'],
+                            pygame.Rect(p.x + block_border_size, 
+                                        p.y + block_border_size, 
+                                        settings['block_size'] - 2*block_border_size, 
+                                        settings['block_size'] - 2*block_border_size))
+        
+        pygame.draw.rect(self.display,
+                        colours['red'],
+                        pygame.Rect(self.food.x, self.food.y,
+                                    settings['block_size'], 
+                                    settings['block_size']))
+        
+        text = font.render(f'Score: {str(self.score)}', True, colours['white'])
+        self.display.blit(text, [0,0])
+        pygame.display.flip()
     
     def _place_food(self):
-        pass
+        x_max_scaled = (self.width - settings['block_size']) // settings['block_size']
+        y_max_scaled = (self.height - settings['block_size']) // settings['block_size']
+        x = random.randint(0, x_max_scaled) * settings['block_size']
+        y = random.randint(0, y_max_scaled) * settings['block_size']
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self._place_food()
 
 if __name__ == '__main__':
     game = SnakeGame()
